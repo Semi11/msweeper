ae58f6a30f729f4e9b9fbd9e118338e03925d5d0
semi11 <stand.say.me@gmai.com>
2018-05-08 14:12:13 +0900

    座標を描画する処理を追加

--- Src/ms_view.cpp
+++ Src/ms_view.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
 #include "ms_view.hpp"
 
 void View::dispBoard(const std::vector<CellState> &cellsState, int height, int width)const{
+  std::cout << "   ";
+  for(int i=0;i<width;i++){
+    std::cout << i/10 << i%10 << ' ';
+  }
+  std::cout << std::endl;
+  std::cout << "   ";
+  for(int i=0;i<width;i++)std::cout << "___";
+  std::cout << std::endl;
+
   for(int i=0;i<height;i++){
+    std::cout << i/10 << i%10 << '|';
     for(int j=0;j<width;j++){
-      std::cout << VIEW_CELL_LIST.at(cellsState.at(i * width + j));
+      std::cout << ' ' << VIEW_CELL_LIST.at(cellsState.at(i * width + j)) << ' ';
     }
     std::cout << std::endl;
   }
